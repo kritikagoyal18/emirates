9436c4a117db404fe7bea99c5e106307c20695d6
Kritika Goyal <kritikagoyal@adobe.com>
2025-09-16 09:55:13 +0530

    Locations component styling

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -11,24 +11,27 @@ const Locations = ({ items = [] }) => {
 
   return (
     <div className="locations container">
-      {items.map((cf, index) => (
-        <div key={cf?._path || index} className="location-row">
-          <ContentFragment cf={cf} className="location-inner" behavior="component" label="Location">
-            {cf?.image && (
-              <Image src={cf?.image?._path} prop="image" label="Image" className="location-card__image" />
-            )}
-            <div className="location-card__panel">
-              {cf?.pretitle && (
-                <span className="location-card__country">{cf.pretitle}</span>
+      <h2 className="locations__heading">Featured destinations from India</h2>
+      <div className="locations-grid">
+        {items.map((cf, index) => (
+          <div key={cf?._path || index} className="location-row">
+            <ContentFragment cf={cf} className="location-inner" behavior="component" label="Location">
+              {cf?.image && (
+                <Image src={cf?.image?._path} prop="image" label="Image" className="location-card__image" />
               )}
-              <Title heading="h3" prop="title" label="Title" className="location-card__title">
-                {cf?.title}
-              </Title>
-              <Text content={cf?.description} prop="description" label="Description" className="location-card__desc" />
-            </div>
-          </ContentFragment>
-        </div>
-      ))}
+              <div className="location-card__panel">
+                {cf?.pretitle && (
+                  <span className="location-card__country">{cf.pretitle}</span>
+                )}
+                <Title heading="h3" prop="title" label="Title" className="location-card__title">
+                  {cf?.title}
+                </Title>
+                <Text content={cf?.description} prop="description" label="Description" className="location-card__desc" />
+              </div>
+            </ContentFragment>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
